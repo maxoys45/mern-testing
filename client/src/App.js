86978219fc79307e390f87059f6d3e6376808a9b
Max Henchman <max.henchman@mous.co>
2020-04-26 18:00:36 +0100

    - server keeps breaking on restart saying port is in use, not sure why server isn't stopping when i make a change

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@ import store from './store'
 import { GlobalProvider } from "./context/GlobalState";
 import { loadUser } from './actions/authActions'
 
-import AuthContainer from './components/auth/AuthContainer'
 import { AddItem } from './components/AddItem'
 import { Items } from './components/ItemList'
 
@@ -20,7 +19,6 @@ export default class App extends Component {
         <>
           <h1>My App</h1>
 
-          {/* <AuthContainer /> */}
           <AddItem />
           <Items />
         </>
